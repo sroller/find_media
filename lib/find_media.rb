c42a9f5fd29425c5f060dc2207274303b715fd67
Steffen Roller <steffen.roller@gmail.com>
2017-03-22 17:27:56 -0400

    playing with modules

--- lib/find_media.rb
+++ lib/find_media.rb
@@ -2,5 +2,15 @@
 
 module FINDMEDIA
 
+def self.videos
+	puts "#{ENV['HOMEDRIVE']}#{ENV['HOMEPATH'].gsub /\\/, '/'}/videos/**/*.mts"
+	Dir["#{ENV['HOMEDRIVE']}#{ENV['HOMEPATH'].gsub /\\/, '/'}/videos/**/*.MTS"]
+end
+
+
+if __FILE__ == $0
+	v = videos
+	puts v.size
+end
 
 end # module
